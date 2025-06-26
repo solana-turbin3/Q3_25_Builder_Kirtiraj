8dba77c5f41e5244d9d4a5ce12d49af3e3318911
kirtiraj22 <kirtirajthakor064@gmail.com>
2025-06-26 12:17:31 +0530

    TASK 3 : Transfer SOL from dev wallet to Turbin3 wallet

---
+++ airdrop/transfer.ts
@@ -0,0 +1,47 @@
+import {
+	Transaction,
+	SystemProgram,
+	Connection,
+	Keypair,
+	LAMPORTS_PER_SOL,
+	sendAndConfirmTransaction,
+	PublicKey,
+} from "@solana/web3.js";
+
+import wallet from "./dev-wallet.json";
+
+const from = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+const to = new PublicKey("BSgVj3bfNVoPNvWLGBn4LHaUwUoRSRS6H2NUP2GHXZKd");
+
+const connection = new Connection("https://api.devnet.solana.com");
+
+(async () => {
+	try {
+		const transaction = new Transaction().add(
+			SystemProgram.transfer({
+				fromPubkey: from.publicKey,
+				toPubkey: to,
+				lamports: LAMPORTS_PER_SOL / 100,
+			})
+		);
+
+		transaction.recentBlockhash = (
+			await connection.getLatestBlockhash("confirmed")
+		).blockhash;
+
+		transaction.feePayer = from.publicKey;
+
+		const signature = await sendAndConfirmTransaction(
+			connection,
+			transaction,
+			[from]
+		);
+
+		console.log(
+			`Success! Checkout your TX here: https://explorer.solana.com/tx/${signature}?cluster=devnet`
+		);
+	} catch (error) {
+		console.error(`Oops something went wrong: ${error}`);
+	}
+})();
